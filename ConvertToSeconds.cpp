ccd8abbd9511deb138f7617ad9016c1ce1435113
adyanrahman5436 <117797159+adyanrahman5436@users.noreply.github.com>
2023-04-17 18:16:35 -0500

    Add files via upload

---
+++ ConvertToSeconds.cpp
@@ -0,0 +1,61 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    long long int days, hours, minutes, seconds, remainSeconds;
+    cout << "enter number of seconds: ";
+    cin >> seconds;
+    
+    if(seconds <= 0)
+    {
+        cout << "seconds must be greater than zero" << endl;
+        exit(-1);
+    }
+    
+    days = seconds / (24 * 3600);
+    
+    seconds = seconds % (24 * 3600);
+   
+    hours = seconds / 3600;
+    
+    seconds = seconds % 3600;
+    
+    minutes = seconds / 60;
+    
+    seconds = seconds % 60;
+    
+    remainSeconds = seconds;
+    
+    if(days >= 1)
+    {
+    cout << "Day(s): " << days << endl;
+    }
+    if(hours >= 1)
+    {
+    cout << "Hour(s): " << hours << endl;
+    }
+    if(minutes >= 1)
+    {
+    cout << "Minute(s): " << minutes << endl;
+    }
+    if(seconds >= 1)
+    {
+    cout << "Remaining second(s): " << remainSeconds << endl;
+    }
+    
+
+    return 0;
+}
+
